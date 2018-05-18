d6616f462712c5be835d53abe8c7bc569d42ad92
FULLNAM-0RKGSQI\Administrator <jyk0802@gmail.com>
2018-05-18 17:49:59 +0900

    카메라 위치 pickup 물체

---
+++ Roll a Ball/Assets/Scripts/CameraMover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour {
+
+    private Transform playerTransform;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        playerTransform = player.GetComponent<Transform>();
+        offset = transform.position - playerTransform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = offset + playerTransform.position;
+	}
+}
---
+++ Roll a Ball/Assets/Scripts/Pickup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
