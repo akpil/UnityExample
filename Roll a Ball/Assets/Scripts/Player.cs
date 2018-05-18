a87c44198f91f7b8c30f8f21db863ab2d3f583b4
FULLNAM-0RKGSQI\Administrator <jyk0802@gmail.com>
2018-05-18 17:20:18 +0900

    공굴리기 플레이어 움직임, 벽, 바닥 설정

---
+++ Roll a Ball/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(horizontal, 0, vertical) * 5;
+        //rb.AddForce(new Vector3(horizontal, 0, vertical) * 5);
+    }
+}
