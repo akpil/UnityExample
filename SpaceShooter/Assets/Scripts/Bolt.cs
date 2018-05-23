7a4b7ddf321151f32dcd6d7bccfcb70ca82971a8
FULLNAM-0RKGSQI\Administrator <jyk0802@gmail.com>
2018-05-23 17:50:05 +0900

    플레이어 위치 클램프 & 틸팅, 총알 생성,

---
+++ SpaceShooter/Assets/Scripts/Bolt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bolt : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float velocity;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.up * velocity;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- SpaceShooter/Assets/Scripts/PlayerController.cs
+++ SpaceShooter/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    Rigidbody rb;
-
+    private Rigidbody rb;
+    public float Speed;
+    public float xMin, xMax, zMin, zMax;
+    public float tiltAngle;
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
@@ -16,6 +18,14 @@ public class PlayerController : MonoBehaviour {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        rb.velocity = new Vector3(horizontal, 0, vertical) * 10;
+        rb.velocity = new Vector3(horizontal, 0, vertical) * Speed;
+
+        Debug.Log(horizontal);
+
+        transform.rotation = Quaternion.Euler(0,0, -horizontal * tiltAngle);
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, xMin, xMax)
+                                        ,0
+                                        , Mathf.Clamp(transform.position.z, zMin, zMax));
 	}
 }
