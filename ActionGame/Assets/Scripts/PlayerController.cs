b3065228e021213bb31c8c2f7ee4bb3cd40c4ef9
FULLNAM-0RKGSQI\Administrator <jyk0802@gmail.com>
2018-06-04 17:52:04 +0900

    캐릭터 겹침이동기능, 플레이어 조작,좀비 애니메이션 스테이트머신 코드작성

---
+++ ActionGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    public void LeftClick()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+    }
+    public void RightClick()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+    }
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            LeftClick();
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            RightClick();
+        }
+	}
+}
--- ActionGame/Assets/Scripts/ZombieMover.cs
+++ ActionGame/Assets/Scripts/ZombieMover.cs
@@ -2,34 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ZombieState
+{
+    Idle, Move, Attack, Dead
+};
+
 public class ZombieMover : MonoBehaviour {
 
     private Rigidbody2D rb;
     private Animator anim;
-    public float speed;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private Transform HPLocation;
 
+    private ZombieState state;
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        StartCoroutine(WalkState());
+        state = ZombieState.Move;
+        StartCoroutine(State());
 	}
 
-    private IEnumerator WalkState()
+    private IEnumerator State()
     {
         while (true)
         {
-            rb.velocity = transform.right * speed;
-            anim.SetBool(AnimationHashList.IsWalkAnimHash, true);
-            yield return new WaitForSeconds(.2f);
-            rb.velocity = Vector3.zero;
-            anim.SetBool(AnimationHashList.IsWalkAnimHash, false);
-            yield return new WaitForSeconds(.2f);
+            switch(state)
+            {
+                case ZombieState.Move:
+                    rb.velocity = transform.right * speed;
+                    anim.SetBool(AnimationHashList.IsWalkAnimHash, true);
+                    yield return new WaitForSeconds(.2f);
+                    rb.velocity = Vector3.zero;
+                    anim.SetBool(AnimationHashList.IsWalkAnimHash, false);
+                    yield return new WaitForSeconds(.2f);
+                    break;
+                case ZombieState.Attack:
+                    anim.SetBool(AnimationHashList.AttackAnimHash, true);
+                    yield return new WaitForSeconds(1f);
+                    anim.SetBool(AnimationHashList.AttackAnimHash, false);
+                    yield return new WaitForSeconds(1f);
+                    break;
+            }
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            state = ZombieState.Attack;
+            anim.SetBool(AnimationHashList.IsWalkAnimHash, false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
