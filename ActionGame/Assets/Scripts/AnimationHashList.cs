946dfb9de7f856912a3292575612926f8ad571aa
FULLNAM-0RKGSQI\Administrator <jyk0802@gmail.com>
2018-06-04 16:50:23 +0900

    Animation 컨트롤 세팅

---
+++ ActionGame/Assets/Scripts/AnimationHashList.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationHashList : MonoBehaviour {
+
+    public static int IsWalkAnimHash;
+    public static int IsDeadAnimHash;
+    public static int AttackAnimHash;
+
+    private void Awake()
+    {
+        IsDeadAnimHash = Animator.StringToHash("IsDead");
+        IsWalkAnimHash = Animator.StringToHash("IsWalk");
+        AttackAnimHash = Animator.StringToHash("Attack");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ ActionGame/Assets/Scripts/ZombieMover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieMover : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        StartCoroutine(WalkState());
+	}
+
+    private IEnumerator WalkState()
+    {
+        while (true)
+        {
+            rb.velocity = transform.right * speed;
+            anim.SetBool(AnimationHashList.IsWalkAnimHash, true);
+            yield return new WaitForSeconds(.2f);
+            rb.velocity = Vector3.zero;
+            anim.SetBool(AnimationHashList.IsWalkAnimHash, false);
+            yield return new WaitForSeconds(.2f);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
