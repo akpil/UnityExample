76bf3b31d3ae411f39996e568e3235a1129c46ce
FULLNAM-0RKGSQI\Administrator <jyk0802@gmail.com>
2018-06-12 17:30:05 +0900

    복잡한 UI컨트롤러

--- ActionGame/Assets/Scripts/LobbyUIController.cs
+++ ActionGame/Assets/Scripts/LobbyUIController.cs
@@ -1,18 +1,96 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-public class LobbyUIController : MonoBehaviour {
 
+
+// 원래 이자리에 있으면 안됨
+[Serializable]
+public class ItemData
+{
+    public int price;
+    public int ElementID;
+    public string title, contetns;
+}
+
+public class LobbyUIController : MonoBehaviour {
+    public static LobbyUIController inst;
     [SerializeField]
     private Button GameStartButton;
 
+    [SerializeField]
+    ItemData[] ItemList;
+    [SerializeField]
+    Sprite[] ItemIcon;
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField]
+    ScrollUIElement[] ScrollElements;
+
+    [SerializeField]
+    private int money;
+
+    private void Awake()
+    {
+        inst = this;
+        // 실제론 안쓰는 코드 예제임
+        ItemList = new ItemData[3];
+        for (int i = 0; i < ItemList.Length; i++)
+        {
+            ItemList[i] = new ItemData();
+        }
+        ItemList[0].title = "사막";
+        ItemList[1].title = "공동묘지";
+        ItemList[2].title = "설원";
+
+        ItemList[0].contetns = "사막맵입니다.";
+        ItemList[1].contetns = "묘지맵입니다.";
+        ItemList[2].contetns = "설원맵입니다.";
+
+        ItemList[0].price = 100;
+        ItemList[1].price = 200;
+        ItemList[2].price = 300;
+
+        ItemList[0].ElementID = 0;
+        ItemList[1].ElementID = 1;
+        ItemList[2].ElementID = 2;
+    }
+
+    // Use this for initialization
+    void Start () {
         GameStartButton.onClick.AddListener(StartGame);
+        for (int i = 0; i < ScrollElements.Length; i++)
+        {
+            ScrollElements[i].SetData(ref ItemList[i]);
+            ScrollElements[i].Renew(money);
+        }
+    }
 
+    public void AddMoney()
+    {
+        money += 50;
+        UIRenew();
+    }
+
+    public Sprite GetSprite(int id)
+    {
+        return ItemIcon[id];
+    }
+
+    public void UIRenew()
+    {
+        for (int i = 0; i < ScrollElements.Length; i++)
+        {
+            ScrollElements[i].Renew(money);
+        }
+    }
+
+    public void PurchaseItem(int id)
+    {
+        money -= ItemList[id].price;
+
+        UIRenew();
     }
 
     public void StartGame()
---
+++ ActionGame/Assets/Scripts/ScrollUIElement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollUIElement : MonoBehaviour {
+
+    [SerializeField]
+    Image Icon;
+
+    [SerializeField]
+    Button purchaseButton;
+
+    [SerializeField]
+    Text title, priceText, contents;
+
+    private int price;
+
+    [SerializeField]
+    int ElementID;
+
+    public void SetData(ref ItemData data)
+    {
+        purchaseButton.interactable = false;
+        title.text = data.title;
+
+        price = data.price;
+        priceText.text = price.ToString();
+
+        contents.text = data.contetns;
+        ElementID = data.ElementID;
+        Icon.sprite = LobbyUIController.inst.GetSprite(ElementID);
+
+        purchaseButton.onClick.AddListener(Purcahse);
+            //() => { LobbyUIController.inst.PurchaseItem(ElementID); });
+    }
+
+    public void Renew(int money)
+    {
+        if (price <= money)
+        {
+            purchaseButton.interactable = true;
+        }
+        else
+        {
+            purchaseButton.interactable = false;
+        }
+    }
+
+    private void Purcahse()
+    {
+        purchaseButton.gameObject.SetActive(false);
+        LobbyUIController.inst.PurchaseItem(ElementID);
+    }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
